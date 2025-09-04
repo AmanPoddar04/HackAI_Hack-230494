151837b68789112d3b69c93a9f84bf5e5f947b14
AmanPoddar04 <bingeallnight04@gmail.com>
2025-09-05 01:45:50 +0530

    Added comments to messages

--- src/messages/weather.py
+++ src/messages/weather.py
@@ -1,6 +1,8 @@
 from uagents import Model
 
-
+# Define a model for weather requests
 class WeatherRequest(Model):
+    # Latitude of the location for the weather request
     latitude: float
+    # Longitude of the location for the weather request
     longitude: float
